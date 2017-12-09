629629c98a3f70bd206defd53ed8ee8381d9cff0
Anna Japko <ebingeranna@gmail.com>
2017-12-09 12:42:21 +0100

    I changed ex11

--- ex11_praticepython.py
+++ ex11_praticepython.py
@@ -1,15 +1,24 @@
 # http://www.practicepython.org/exercise/2014/04/16/11-check-primality-functions.html
 #ex11
+import sys
 def prime():
-    return int(input("Wybierz liczbe: "))
+    return int(input("Type number: "))
 
-liczba = prime()
-if liczba>1:    
-    for i in range(2,liczba):
-        if liczba%i==0:
-            print("to nie jest liczba pierwsza")
-            break
-        else:
-            print("to jest liczba pierwsza")
-            break
+number = prime()
+if number>1:    
+    for i in range(2,number):
+        if number%i!=0:
+            continue
+        elif number%i==0:
+            sys.exit("This number isn't prime.")
+            
+    sys.exit("This number is prime")
+
+elif number==0:
+    sys.exit("You typed zero, could you type other number?")
+
+""" When I changed this exercise I learnt a new library sys and command sys.exit.
+ I think that in this exercise this command has the same function as print and break.
+ Is it ok for you?"""         
         
+#I changed variables from Polish to English
