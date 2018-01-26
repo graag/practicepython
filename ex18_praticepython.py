433aeef96a6c6424cf06d6895f3f5913f1b865c2
Anna Japko <ebingeranna@gmail.com>
2018-01-26 10:16:55 +0100

    Wg zasad z wikipedii, czyli porównując liczby zawierające unikalne cyfry, program powinien działać. Pozdrawiam :)

--- ex18_praticepython.py
+++ ex18_praticepython.py
@@ -1,54 +1,39 @@
-#ex18
-#import random
+#ex18   #rules: https://en.wikipedia.org/wiki/Bulls_and_Cows
+import random
 
 def compare(lottery,guess):
-    #first cow (guess in the correct place ) bull (in wrong place)
-    cowbull=[0,0]
+    cowbull=[0,0]   #index zero = bull, index 1 = cow
     
-    for i in range(4):
-      for j in range(4):
-        if guess[i] == lottery[j]:
+    for i in range(len(lottery)):
+      for j in range(len(lottery)):
+        if int(guess[i]) == lottery[j]:
           if i == j:
               cowbull[0]+=1
-          elif guess[j] == lottery[j]:
-              cowbull[1]=cowbull[1]      
-          else:
+              guess[i]=10
+          if i != j:
               cowbull[1]+=1
-#    for i,lottery_val in enumerate(lottery):
-#        #print (i,lottery_val)
-#        for j,guess_val in enumerate(guess):
-#            
-#            if lottery_val==guess_val:
-#                if i==j:
-#                    cowbull[0]+=1
-#                else:
-#                    cowbull[1]+=1
-            
     return cowbull       
 
 if __name__=="__main__":
     playing = True #gotta play the game
-    #lottery=[]
-    #for i in range(4):
-    #    i=random.randint(0,9)
-    #    lottery.append(i)    
+    lottery=random.sample(range(0,9),4)  #random.sample gives me unique digits 
     score = 0
-    lottery2=[6, 1, 6, 8]
 
     while playing:
-        guess2 = input("Give me your best guess!")
-        if guess2 == "exit":
+        guess = list(input("Give me your best guess! The digits must be all different. If the matching digits are in their right positions, they are bulls, if in different positions, they are cows."))
+        guess = [int(i) for i in guess]
+        if guess == "exit":
             break
         else:
-            guess2=list(guess2)
-            cowbullcount = compare(lottery2,guess2)
+            guess=list(guess)
+            cowbullcount = compare(lottery,guess)
             score+=1
                         
-            print("You have "+ str(cowbullcount[0]) + " cows, and " + str(cowbullcount[1]) + " bulls.")
+            print("You have "+ str(cowbullcount[0]) + " bulls, and " + str(cowbullcount[1]) + " cows.")
                 
-            if cowbullcount[1]==4:
+            if cowbullcount[0]==4:
                 playing = False
-                print("You win the game after " + str(score) + "! The number was "+str(lottery2))
+                print("You win the game after " + str(score)+'. '+ 'The number was: '+ str(lottery))
                 break #redundant exit
             else:
                 print("Your guess isn't quite right, try again.")
