c92e48d74ac4d25a7c7abb70b880c2465483a130
Anna Japko <ebingeranna@gmail.com>
2018-02-07 22:14:32 +0100

    Dziękuję za szczegółową instrukcję do zadania 3. Wysyłam zrobione zadanie :) Pozdrawiam

--- ex3_praticepython.py
+++ ex3_praticepython.py
@@ -1,15 +1,19 @@
 # changed ex3 praticepython.org
 
 import json
-array={}
-array = '{"numbers": [1,1,2,3,5,8,13,21,34,55,89]}'
-data  = json.loads(array)
-array2 = data['numbers']
-print(array2)
-s=json.dumps(array2)
-with open("array.txt","w") as f:
-    f.write(s)
-with open("array.txt","r") as f:
-    array3=json.load(f)
-typ=type(array3)   
-print(typ)
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('file', type=argparse.FileType('r'))
+args = parser.parse_args()
+
+array = [1,1,2,3,5,8,13,21,34,55,89]
+
+with open("array.json","w") as f:
+    json.dump(array,f)    
+
+with open("array.json") as f:
+    data=json.load(f)
+    for i in data:
+        if i<5:
+            print(i)
