18612c9180b6e01d0e482f9f3e49d0d621c02225
Anna Japko <ebingeranna@gmail.com>
2018-02-15 13:50:24 +0100

    Cześć, przesyłam przeprawiony wg wzoru ex3 Pozdrawiam, Ania Ebinger

--- ex3_praticepython.py
+++ ex3_praticepython.py
@@ -1,20 +1,12 @@
 # changed ex3 praticepython.org
 import argparse
-import string as st
+import json
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--input","-i",help="file", type=argparse.FileType('r'))
+parser.add_argument("--input","-i",help="file", required=True)
 args=parser.parse_args()
-sas=args.input.read()
-
-sas.translate(st.punctuation)
-print(sas)
-sas=sas[1:(len(sas)-1)]
-sas2 = sas.split(',')
-str.maketrans
-sas2 = [int(i) for i in sas2]
-print(sas2)
-print(type(sas2))
-for j in sas2:
-    if j<5:
-        print(j)
+with open(args.input) as f:
+    data = json.load(f)
+    for j in data:
+        if j<5:
+            print(j)
